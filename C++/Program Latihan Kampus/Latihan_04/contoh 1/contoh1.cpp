2079cb40ee256c2f80e8b9dddb05939875300424
CallMeRyd <aryaandhika222@gmail.com>
2024-11-02 06:37:37 +0700

    advertisement update dan latihan CPP latihan 4

---
+++ C++/Program Latihan Kampus/Latihan_04/contoh 1/contoh1.cpp
@@ -0,0 +1,17 @@
+// contoh 1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	a = 10;
+	b = 4;
+	a = b;
+	b = 7;
+	
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	
+	return 0;
+}
